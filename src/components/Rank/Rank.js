9cbaa3b6da476866fcfb10bd60ddf6d7bcc6c7df
Louis Steimel <lhsthree@gmail.com>
2020-05-14 21:18:49 -0500

    initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div className = ''>
+			<div className = 'white f3'>
+				{'Louis, Your current rank :'}
+			</div>
+			<div className = 'white f1'>
+				{'No. 5'}
+			</div>
+		</div>
+		);
+}
+
+export default Rank ;
