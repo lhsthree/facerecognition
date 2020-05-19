e783b3ffb4b7bf3652e930b20c68ed5728a134e1
Louis Steimel <lhsthree@gmail.com>
2020-05-18 23:56:45 -0500

    push before deployment

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -30,9 +30,7 @@ class SignIn extends React.Component  {
 		.then(user => {
 			if(user.id){
 				this.props.loadUser(user);
-				this.props.onRouteChange('home')
-	
-				
+				this.props.onRouteChange('home')	
 			}
 		})
 	}
